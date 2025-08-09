f52228638519921de0d46d3c2abbe32e03816e94
Timmy Adeyeloja <adefeyitimi@gmail.com>
2025-08-09 18:54:15 +0100

    feat: implement helper to deliver transactional emails

--- src/helpers/email/index.ts
+++ src/helpers/email/index.ts
@@ -1,27 +1,34 @@
+import { MailtrapClient } from 'mailtrap';
 
-import { Resend } from 'resend';
-import * as fs from 'fs';
-import * as path from 'path';
+const client = new MailtrapClient({
+    token: process.env.MAILTRAP_API_KEY!,
+})
 
-const resend = new Resend(process.env.RESEND_API_KEY);
 
-export const sendEmail = async (to: string, subject: string, templateName: string, data: Record<string, string>) => {
-    const templatePath = path.join(__dirname, `../../templates/${templateName}.html`);
-    let html = fs.readFileSync(templatePath, 'utf-8');
+export const sendEmail = async (to: string, subject: string, body: string) => {
+    const maxRetries = 3;
+    let retries = 0;
 
-    for (const key in data) {
-        const regex = new RegExp(`{{${key}}}`, 'g');
-        html = html.replace(regex, data[key]);
+    while (retries < maxRetries) {
+        try {
+            await client.send({
+                from: {
+                    name: "LendBloc",
+                    email: process.env.MAILTRAP_FROM_EMAIL!
+                },
+                to: [{ email: to }],
+                subject,
+                html: body
+            })
+            break;
+        } catch (error) {
+            console.error('Error sending email:', error);
+            retries++;
+            await new Promise(resolve => setTimeout(resolve, 1000));
+        }
     }
 
-    try {
-        await resend.emails.send({
-            from: 'LendBloc <onboarding@resend.dev>',
-            to,
-            subject,
-            html,
-        });
-    } catch (error) {
-        console.error('Error sending email:', error);
+    if (retries === maxRetries) {
+        console.error('Failed to send email after', maxRetries, 'retries');
     }
-};
+}
