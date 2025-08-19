4897621f16fed7a551a8dda25bcc2bd3faca461c
Timmy Adeyeloja <adefeyitimi@gmail.com>
2025-08-19 13:19:04 +0100

    chore: remove callback and redirect url

--- src/helpers/shufti/index.ts
+++ src/helpers/shufti/index.ts
@@ -10,18 +10,17 @@ const AUTH_TOKEN = Buffer.from(`${SHUFTI_PRO_CLIENT_ID}:${SHUFTI_PRO_CLIENT_SECR
 export const verifyUser = async (kycReferenceId: string) => {
     let payload = {
         reference: kycReferenceId,
-        callback_url: `${process.env.PORT_FORWARD_URL}/api/v1/webhooks/shufti/callback`,
-        redirect_url: `${process.env.PORT_FORWARD_URL}/api/v1/webhooks/shufti/redirect`,
+        // callback_url: `${process.env.PORT_FORWARD_URL}/api/v1/webhooks/shufti/callback`,
+        // redirect_url: `${process.env.PORT_FORWARD_URL}/api/v1/webhooks/shufti/redirect`,
         country: "GB",
         language: "EN",
         verification_mode: "any", // Can be 'any' or 'all'
-        ttl: 60,
+        ttl: 60, // Time to live in seconds
         allow_warnings: "1",
         document: {
-            proof: ["front", "back"],
             supported_types: ["id_card", "passport", "driving_license"],
             allow_unsupported_document: "0",
-            allow_offline: "0",
+            allow_offline: "1",
             allow_photocopy: "0",
             allow_scanned: "0",
             allow_screenshot: "0",
@@ -29,27 +28,19 @@ export const verifyUser = async (kycReferenceId: string) => {
             age: {
                 min: 18,
                 max: 100
-            }
+            },
+            backside_proof_required: "0",
+            allow_upload: "1"
         },
         face: {
-            proof: ["live"],
-            allow_offline: "0",
+            allow_offline: "1",
             allow_photocopy: "0",
             allow_scanned: "0",
             allow_screenshot: "0",
-            allow_padding: "0"
+            allow_padding: "0",
+            allow_online: "0"
         },
-        address: {
-            proof: ["any"],
-            supported_types: ["utility_bill", "bank_statement", "other"],
-            allow_unsupported_document: "0",
-            allow_offline: "0",
-            allow_photocopy: "0",
-            allow_scanned: "0",
-            allow_screenshot: "0",
-            allow_id_mismatch: "0"
-        }
-    }
+    };
 
     try {
         const response = await axios.post(SHUFTI_BASE_URL, payload, {
@@ -62,6 +53,27 @@ export const verifyUser = async (kycReferenceId: string) => {
         return response.data;
     } catch (error: any) {
         console.error('Error verifying user:', error.response.data);
+        throw error;
+    }
+}
+
+export const getVerificationStatus = async (kycReferenceId: string) => {
+    try {
+
+        let payload = {
+            reference: kycReferenceId
+        }
+
+        const response = await axios.post("https://api.shuftipro.com/status", payload, {
+            headers: {
+                'Authorization': `Basic ${AUTH_TOKEN}`,
+                'Content-Type': 'application/json'
+            },
+        });
+
+        return response.data;
+    } catch (error: any) {
+        console.error('Error getting verification status:', error.response.data);
         throw error;
     }
 }
