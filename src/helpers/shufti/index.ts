41d139dd5cae8cae136e81ea50662e767c232c03
Timmy Adeyeloja <adefeyitimi@gmail.com>
2025-08-19 22:08:36 +0100

    style(shufti): Use type-only imports
    This commit updates the import statement in the Shufti Pro helper to use type-only imports for Axios types, which is a good practice in TypeScript.

--- src/helpers/shufti/index.ts
+++ src/helpers/shufti/index.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosError, AxiosInstance } from "axios";
+import axios, { type AxiosError, type AxiosInstance } from "axios";
 import crypto from "crypto";
 
 export enum ShuftiType {
